e55dcc395e2476be668b3c8be52a95c2de666c49
Uuuuuumm <tateo9119@gmail.com>
2022-03-12 21:07:04 -0500

    item equip correction

--- Assets/Scripts/EquipmentManager.cs
+++ Assets/Scripts/EquipmentManager.cs
@@ -35,72 +35,77 @@ public class EquipmentManager : MonoBehaviour
 
     void Start()
     {
-        
-        inventory = Inventory.instance;
-        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
-        currentEquipment = new Equipment[numSlots];
+        if (!GameManager.instance.isMultiplayer || gameObject.GetComponent<PhotonView>().IsMine)
+        { 
+            inventory = Inventory.instance;
+            int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+            currentEquipment = new Equipment[numSlots];
 
 
-        canvas = GameObject.Find("Canvas");
+            canvas = GameObject.Find("Canvas");
 
-        weaponHolder = gameObject.transform.GetChild(1).GetChild(16).GetChild(0).GetChild(6).GetChild(0).GetChild(3).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetChild(3);
+            //weaponHolder = gameObject.transform.GetChild(1).GetChild(16).GetChild(0).GetChild(6).GetChild(0).GetChild(3).GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetChild(3);
 
 
-        weaponSlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(0).GetComponent<InventorySlot>();
-        spellSlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(1).GetComponent<InventorySlot>();
-        AbilitySlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(2).GetComponent<InventorySlot>();
+            weaponSlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(0).GetComponent<InventorySlot>();
+            spellSlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(1).GetComponent<InventorySlot>();
+            AbilitySlot = canvas.transform.GetChild(2).GetChild(3).GetChild(0).GetChild(2).GetComponent<InventorySlot>();
+        }
     }
 
         // Equip a new Item
     public void Equip (Equipment newItem)
     {
-        // Find what slot the item fits
-        int slotIndex = (int)newItem.equipSlot;
-
-        Equipment oldItem = null;
-
-        // If there's an item already in the slot
-        // Swap with this item
-        if (currentEquipment[slotIndex] != null)
+        if (!GameManager.instance.isMultiplayer || gameObject.GetComponent<PhotonView>().IsMine)
         {
-            oldItem = currentEquipment[slotIndex];
-            inventory.Add(oldItem);
-            /*if (GameManager.instance.isMultiplayer)
-                PhotonNetwork.Destroy(weaponHolder.GetChild(0).gameObject);
-            else*/
+            // Find what slot the item fits
+            int slotIndex = (int)newItem.equipSlot;
+
+            Equipment oldItem = null;
+
+            // If there's an item already in the slot
+            // Swap with this item
+            if (currentEquipment[slotIndex] != null)
+            {
+                oldItem = currentEquipment[slotIndex];
+                inventory.Add(oldItem);
+                /*if (GameManager.instance.isMultiplayer)
+                    PhotonNetwork.Destroy(weaponHolder.GetChild(0).gameObject);
+                else*/
                 Destroy(weaponHolder.GetChild(0).gameObject);
-        }
+            }
 
-        // An item has been equipped
-        if (onEquipmentChanged != null)
-        {
-            onEquipmentChanged.Invoke(newItem, oldItem);
-        }
+            // An item has been equipped
+            if (onEquipmentChanged != null)
+            {
+                onEquipmentChanged.Invoke(newItem, oldItem);
+            }
 
-        // Insert item into the slot
-        currentEquipment[slotIndex] = newItem;
+            // Insert item into the slot
+            currentEquipment[slotIndex] = newItem;
 
-        // Equip Weapon
-        if (slotIndex == 0)
-        {
-            /// Put the Sword in the player's Hand
-            Vector3 pos = weaponHolder.position;
+            // Equip Weapon
+            if (slotIndex == 0)
+            {
+                /// Put the Sword in the player's Hand
+                Vector3 pos = weaponHolder.position;
 
-            // Create new Item and place it in the weapon holder
-            GameObject Sword = Instantiate(newItem.prefab, pos, Quaternion.identity, weaponHolder);
-            Sword.gameObject.GetComponent<BoxCollider>().enabled = false;
-            Sword.gameObject.GetComponent<ItemPickup>().enabled = false;
-            Sword.gameObject.name = "weapon";
+                // Create new Item and place it in the weapon holder
+                GameObject Sword = Instantiate(newItem.prefab, pos, Quaternion.identity, weaponHolder);
+                Sword.gameObject.GetComponent<BoxCollider>().enabled = false;
+                Sword.gameObject.GetComponent<ItemPickup>().enabled = false;
+                Sword.gameObject.name = "weapon";
 
-            weaponSlot.AddItem(newItem);
-        }
-        else if (slotIndex == 1)
-        {
-            spellSlot.AddItem(newItem);
-        }
-        else if(slotIndex == 2)
-        {
-            AbilitySlot.AddItem(newItem);
+                weaponSlot.AddItem(newItem);
+            }
+            else if (slotIndex == 1)
+            {
+                spellSlot.AddItem(newItem);
+            }
+            else if (slotIndex == 2)
+            {
+                AbilitySlot.AddItem(newItem);
+            }
         }
 
     }
